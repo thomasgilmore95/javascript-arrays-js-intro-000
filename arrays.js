5eb8865ad4f45639f952ce72c4639860a8906ea6
Thomas Gilmore <tomtitans@aol.com>
2019-10-29 14:47:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,32 +3,3 @@ var chocolateBars = ["Snickers", "hundred grand", "kitkat", "skittles"];
    var new_array = [element, array[0]]
    return new_array
  }
-
- function descructivelyAddElementToBeginningOfArray(array, element){
-   array.unshift(element, array)
-   return array
- }
-
- function addElementToEndOfArray(array, element){
-   array.push(element)
-   return array
- }
-
- function destructivelyAddElementToEndOfArray(array, element){
-   array.push(element)
-   return array
- }
-
- function accessElementInArray(array, index){
-   return array[2]
- }
-
- function removeElementFromBeginningOfArray(array){
-   array.shift()
-   return array
- }
-
- function removeElementFromEndOfArray(array){
-   array.pop()
-   return array
- }
